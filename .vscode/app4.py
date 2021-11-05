0a64aa3fd20a422eb54933f76b75ea842cfdc840
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-11-05 23:26:35 +0530

    IF ELSE

---
+++ .vscode/app4.py
@@ -0,0 +1,30 @@
+is_hot=False
+is_cold=False
+
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+
+elif is_cold:
+   print("It's a cold day")
+   print("Wear warm clothes")
+
+else:
+    print("It's a lovely day")
+
+   
+
+print("Enjoy your day")
+    
+
+price=1000000
+is_credit=True
+
+if is_credit:
+    price=0.9*price
+
+else: 
+    price=0.8*price
+
+
+print(f"payment: ${price}")
