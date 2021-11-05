d121e3415dc6ce3fd004945978faedfe019d8469
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-11-05 18:43:38 +0530

    second commit

--- .vscode/app2.py
+++ .vscode/app2.py
@@ -5,3 +5,15 @@ print(message)
 
 message2=f'{first} [{last}] is a coder'
 print(message2)
+course="prithesh"
+print(course.upper())
+course=course.upper()
+print(course)
+print(course.lower())
+print(course.find('P'))
+print(course.find('x'))
+print(course.replace('PRITHESH','hahaha'))
+
+print('aha' in course)
+print('PRI' in course)
+
