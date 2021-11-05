1d023733fce1fb8eb486592d7defea2117e89bfc
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-11-05 16:07:50 +0530

    first commit

---
+++ .vscode/app.py
@@ -0,0 +1,27 @@
+
+
+
+birth_year=int(input('Birth year: '))
+print(type(birth_year))
+age=2019-birth_year
+print(age)
+print(type(age))
+
+course='Python Course for "Beginners"'
+
+course='''
+
+Hi Prithesh
+This is the first email
+Thank You
+The support team
+'''
+
+print(course)
+
+course2='Python for beginners'
+print(course2[0])
+print(course2[-3])
+print(course2[0:3])
+print(course2[1:-1])
+
---
+++ .vscode/app2.py
@@ -0,0 +1,7 @@
+first='John'
+last='Smith'
+message=first+' ['+last+'] is a coder'
+print(message)
+
+message2=f'{first} [{last}] is a coder'
+print(message2)
