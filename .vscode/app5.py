f4272903026f9a20f042bea788b78db6e3224de0
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-11-06 00:04:24 +0530

    in range

---
+++ .vscode/app5.py
@@ -0,0 +1,21 @@
+
+for item in['Mosh','John','Addiee']:
+    print(item)
+
+for i in range(10):
+     print(i)
+
+for i in range(5,10):
+    print(i)
+
+
+prices=[10,20,30]
+
+sum=0
+
+for i in prices:
+    sum+=i
+
+print(f"Total: {sum}")
+
+print(f"Total Sum: {sum}")
